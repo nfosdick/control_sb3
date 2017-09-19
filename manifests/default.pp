146b8e238e1aefb3f8e09e9b0e4763e43fb4c81d
root <root@puppet.lark.com>
2017-09-19 19:12:36 +0000

    test

--- manifests/default.pp
+++ manifests/default.pp
@@ -2,8 +2,8 @@
 # Top Level Manfest: site.pp
 
 node default {
-  if ($::role) {
-    include "roles::${::role}"
+  if ($role) {
+    include "roles::${role}"
   } else {
     warning('There is no "role" parameter specified!!')
   }
